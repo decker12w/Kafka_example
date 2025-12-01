1e5869b56ab638d413eb2242693ae99c462dc2c1
decker12w <josemaia.comp@gmail.com>
2025-12-01 16:44:45 -0300

    Adicionando projetos consumer e producer

---
+++ producer/src/main/java/com/example/demo/PaymentController.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/payment")
+public class PaymentController {
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    @PostMapping("/venda")
+    public String registrarVenda(@RequestBody String venda) {
+        kafkaTemplate.send("SEU_TOPICO_AQUI", venda);
+        return "Venda registrada com sucesso!";
+    }
+}
